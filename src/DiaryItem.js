699a939b622ca6775d901779dd8ec5698294f196
Taesub Yun <taesubyun@icloud.com>
2022-01-08 14:30:27 +0900

    feat : 추가된 일기 리스트 삭제하기

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,11 @@
-const DiaryItem = ({ id, author, content, emotion, created_date }) => {
+const DiaryItem = ({
+    onDelete,
+    id,
+    author,
+    content,
+    emotion,
+    created_date,
+}) => {
     return (
         <div className='DiaryItem'>
             <div className='info'>
@@ -11,6 +18,19 @@ const DiaryItem = ({ id, author, content, emotion, created_date }) => {
                 </span>
             </div>
             <div className='content'>{content}</div>
+            <button
+                onClick={() => {
+                    if (
+                        window.confirm(
+                            `${id}번째 일기를 정말 삭제하시겠습니까?`
+                        )
+                    ) {
+                        onDelete(id);
+                    }
+                }}
+            >
+                삭제하기
+            </button>
         </div>
     );
 };
